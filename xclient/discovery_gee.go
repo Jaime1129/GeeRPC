f94a65b2752768c30ab945f2925a650c3028a77d
jaime.wang <greatjaime.wang@gmail.com>
2021-08-29 21:53:51 +0800

    feat: client fetch server list via discovery

--- xclient/discovery_gee.go
+++ xclient/discovery_gee.go
@@ -1,6 +1,11 @@
 package xclient
 
-import "time"
+import (
+	"log"
+	"net/http"
+	"strings"
+	"time"
+)
 
 type GeeRegistryDiscovery struct {
 	*MultiServersDiscovery
@@ -21,4 +26,53 @@ func NewGeeRegistryDiscovery(registerAddr string, timeout time.Duration) *GeeReg
 		registry: registerAddr,
 		timeout: timeout,
 	}
+}
+
+func (d *GeeRegistryDiscovery) Update(servers []string) error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	d.servers = servers
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GeeRegistryDiscovery) Refresh() error {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.lastUpdate.Add(d.timeout).After(time.Now()) {
+		return nil
+	}
+
+	log.Println("rpc registry: refresh servers from registry", d.registry)
+	resp, err := http.Get(d.registry)
+	if err != nil {
+		log.Println("rpc registry refresh err:", err)
+		return err
+	}
+	servers := strings.Split(resp.Header.Get("X-Geerpc-Servers"), ",")
+	d.servers = make([]string, 0, len(servers))
+	for _, s := range servers {
+		if strings.TrimSpace(s) != "" {
+			d.servers = append(d.servers, strings.TrimSpace(s))
+		}
+	}
+
+	d.lastUpdate = time.Now()
+	return nil
+}
+
+func (d *GeeRegistryDiscovery) Get(mode SelectMode) (string, error) {
+	if err := d.Refresh(); err != nil {
+		return "", err
+	}
+	return d.MultiServersDiscovery.Get(mode)
+}
+
+func (d *GeeRegistryDiscovery) GetAll() ([]string, error) {
+	if err := d.Refresh(); err != nil {
+		return nil, err
+	}
+	return d.MultiServersDiscovery.GetAll()
 }
